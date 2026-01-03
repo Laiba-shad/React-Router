445a5d6367b5888767b7ae39f88a22e138a049b2
Laiba Irshad <laibashad71@gmail.com>
2026-01-03 02:14:02 -0800

    implement navigation between pages and work with MUI library

---
+++ app/homepage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FeaturesSection from '~/components/FeatureSection'
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header/>
+      <FeaturesSection/>
+      <Footer/>
+    </div>
+  )
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
+import { route } from "@react-router/dev/routes";
+export default [
+    index("routes/home.tsx"),
+    route("about", "routes/about.tsx"),
+    route("contact", "routes/contact.tsx"),
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+] satisfies RouteConfig;
